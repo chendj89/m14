50369fee0aff78b69179b32e7619f3bb61bf0662
九转灵山 <chendj89@hotmail.com>
2023-07-02 19:06:50 +0800

    2

---
+++ src/plugins/temp/addRepo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="temp">
+    <div class="temp-upload"></div>
+    <div>
+      <div>仓库名</div>
+      <input type="text">
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts" name="AddRepo"></script>
+
+<style lang="scss" scoped>
+
+
+</style>
