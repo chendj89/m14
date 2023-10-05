1e13f65ab707b024573992f10093d78b38222f0c
九转灵山 <chendj89@hotmail.com>
2023-10-05 16:17:36 +0800

    2323

--- src/components/grid/ts/index.ts
+++ src/components/grid/ts/index.ts
@@ -99,7 +99,7 @@ export const handler = (grid: any, props: any, list: any) => {
     rect = null
     target = null
   }
-  grid.addEventListener('mousedown', (event: any) => {
+  document.addEventListener('mousedown', (event: any) => {
     // event.preventDefault()
     const classList = Array.from(event.target.classList)
     rect = grid.getBoundingClientRect()
@@ -136,54 +136,54 @@ export const handler = (grid: any, props: any, list: any) => {
         target.style.width = lastX - initX + 'px'
         target.style.height = lastY - initY + 'px'
       }
-      if (target && type == 'resize') {
-        let col = Math.floor(
-          (event.pageX - rect.left) / (props.size + props.border)
-        )
-        let row = Math.floor(
-          (event.pageY - rect.top) / (props.size + props.border)
-        )
-        lastX = col * props.size + (col - 1) * props.border
-        lastY = row * props.size + (row - 1) * props.border
-        console.log(lastX,rect.width)
-        if (left + lastX >= rect.width - props.border) {
-          lastX = rect.width - left - props.border
-        }
-      
-        if (top + lastY >= rect.height - props.border) {
-          lastY = rect.height - top - props.border
-        }
-        if (lastX <= 0) {
-          lastX = props.size
-        }
-        if (lastY <= 0) {
-          lastY = props.size
-        }
-        collision(target, props)
-        target.style.width = lastX + 'px'
-        target.style.height = lastY + 'px'
-      }
-      if (target && type == 'move') {
-        let difX = Math.floor((event.pageX - initX) / props.size) * props.size
-        let difY = Math.floor((event.pageY - initY) / props.size) * props.size
-        difX = left + difX
-        difY = top + difY
-        if (difX + rect.width > props.width) {
-          difX = props.width - rect.width
-        }
-        if (difY + rect.height > props.height) {
-          difY = props.height - rect.height
-        }
-        if (difX < 0) {
-          difX = 0
-        }
-        if (difY < 0) {
-          difY = 0
-        }
-        collision(target, props)
-        target.style.left = difX + 'px'
-        target.style.top = difY + 'px'
-      }
+      // if (target && type == 'resize') {
+      //   let col = Math.floor(
+      //     (event.pageX - rect.left) / (props.size + props.border)
+      //   )
+      //   let row = Math.floor(
+      //     (event.pageY - rect.top) / (props.size + props.border)
+      //   )
+      //   lastX = col * props.size + (col - 1) * props.border
+      //   lastY = row * props.size + (row - 1) * props.border
+      //   console.log(lastX,rect.width)
+      //   if (left + lastX >= rect.width - props.border) {
+      //     lastX = rect.width - left - props.border
+      //   }
+
+      //   if (top + lastY >= rect.height - props.border) {
+      //     lastY = rect.height - top - props.border
+      //   }
+      //   if (lastX <= 0) {
+      //     lastX = props.size
+      //   }
+      //   if (lastY <= 0) {
+      //     lastY = props.size
+      //   }
+      //   collision(target, props)
+      //   target.style.width = lastX + 'px'
+      //   target.style.height = lastY + 'px'
+      // }
+      // if (target && type == 'move') {
+      //   let difX = Math.floor((event.pageX - initX) / props.size) * props.size
+      //   let difY = Math.floor((event.pageY - initY) / props.size) * props.size
+      //   difX = left + difX
+      //   difY = top + difY
+      //   if (difX + rect.width > props.width) {
+      //     difX = props.width - rect.width
+      //   }
+      //   if (difY + rect.height > props.height) {
+      //     difY = props.height - rect.height
+      //   }
+      //   if (difX < 0) {
+      //     difX = 0
+      //   }
+      //   if (difY < 0) {
+      //     difY = 0
+      //   }
+      //   collision(target, props)
+      //   target.style.left = difX + 'px'
+      //   target.style.top = difY + 'px'
+      // }
     }
     /**
      * 鼠标结束
@@ -191,7 +191,19 @@ export const handler = (grid: any, props: any, list: any) => {
      */
     const mouseup = (event: MouseEvent) => {
       event.preventDefault()
-      if (target && type == 'create') {
+      if (target && target.parentNode && type == 'create') {
+        let rect2 = target.getBoundingClientRect()
+        if (rect2.right + props.border < rect.left) {
+        }
+        if (rect2.left >= rect.right) {
+          target?.parentNode?.removeChild(target)
+          return
+        }
+        if (rect2.top >= rect.bottom) {
+          target?.parentNode?.removeChild(target)
+          return
+        }
+
         let hasCollision = collision(target, props)
         Array.from(target.parentNode.children).forEach((child: any) => {
           child.style.backgroundColor = props.bg
@@ -218,29 +230,29 @@ export const handler = (grid: any, props: any, list: any) => {
           target.parentNode.removeChild(target)
         }
       }
-      if (target && type == 'resize') {
-        if (collision(target, props) && rect) {
-          target.style.width = rect.width + 'px'
-          target.style.height = rect.height + 'px'
-        }
-        Array.from(target.parentNode.children).forEach((child: any) => {
-          child.style.backgroundColor = props.bg
-        })
-        target.style.zIndex = '1'
-      }
-      if (target && type == 'move') {
-        if (collision(target, props) && rect) {
-          target.style.left = left + 'px'
-          target.style.top = top + 'px'
-        }
-        Array.from(target.parentNode.children).forEach((child: any) => {
-          child.style.backgroundColor = props.bg
-        })
-        target.style.zIndex = '1'
-      }
+      // if (target && type == 'resize') {
+      //   if (collision(target, props) && rect) {
+      //     target.style.width = rect.width + 'px'
+      //     target.style.height = rect.height + 'px'
+      //   }
+      //   Array.from(target.parentNode.children).forEach((child: any) => {
+      //     child.style.backgroundColor = props.bg
+      //   })
+      //   target.style.zIndex = '1'
+      // }
+      // if (target && type == 'move') {
+      //   if (collision(target, props) && rect) {
+      //     target.style.left = left + 'px'
+      //     target.style.top = top + 'px'
+      //   }
+      //   Array.from(target.parentNode.children).forEach((child: any) => {
+      //     child.style.backgroundColor = props.bg
+      //   })
+      //   target.style.zIndex = '1'
+      // }
       reset()
-      grid.removeEventListener('mousemove', mousemove)
-      grid.removeEventListener('mouseup', mouseup)
+      document.removeEventListener('mousemove', mousemove)
+      document.removeEventListener('mouseup', mouseup)
     }
     if (event.target == grid) {
       rect = grid.getBoundingClientRect()
@@ -257,19 +269,19 @@ export const handler = (grid: any, props: any, list: any) => {
       target = createEle({ left: initX, top: initY }, props)
       target.style.zIndex = '2'
       event.target.append(target)
-      document.addEventListener('mousemove', mousemove)
-      document.addEventListener('mouseup', mouseup)
     }
-    if (classList.includes('grid-resize')) {
-      type = 'resize'
-      target = event.target.parentNode
-      //rect = target.getBoundingClientRect()
-      left = parseInt(target.style.left)
-      top = parseInt(target.style.top)
-      target.style.zIndex = '2'
-      document.addEventListener('mousemove', mousemove)
-      document.addEventListener('mouseup', mouseup)
-    }
+    document.addEventListener('mousemove', mousemove)
+    document.addEventListener('mouseup', mouseup)
+    // if (classList.includes('grid-resize')) {
+    //   type = 'resize'
+    //   target = event.target.parentNode
+    //   //rect = target.getBoundingClientRect()
+    //   left = parseInt(target.style.left)
+    //   top = parseInt(target.style.top)
+    //   target.style.zIndex = '2'
+    //   document.addEventListener('mousemove', mousemove)
+    //   document.addEventListener('mouseup', mouseup)
+    // }
     // if (classList.includes('grid-content')) {
     //   type = 'move'
     //   target = event.target.parentNode
